d049e1e466d2616e2dab9af5cacfb684db62473d
Matthew Mitchell <mazchoo@gmail.com>
2023-06-21 15:21:51 +0700

    Nothing bad happens with an empty push

--- BasicToDoList.py
+++ BasicToDoList.py
@@ -7,7 +7,6 @@ from UI.ToDoLayout import Ui_ToDoLayout
 from Models.ToDoModel import ToDoModel
 from Controller.ControlFunctions import ToDoListController
 
-# ToDo - Check what happens when upload is done and there is nothing to push
 # ToDo - Enable backup when there are uncommitted changes
 # ToDo - Allow editing of title to persist in saved data
 # ToDo - Disallow editing of an empty note
