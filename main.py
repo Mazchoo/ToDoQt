6a6f5b34e41c0e7e08e8c35e8b8e339418bfb09e
Matthew Mitchell <mazchoo@gmail.com>
2025-01-04 14:05:06 +0700

    Added back qss editor

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
-from Common.QtHelpers import loadQss
+from Common.QtHelpers import loadQss  # , attachQssEditor
 from UI.ToDoLayout import Ui_ToDoLayout
 from Models.ToDoModel import ToDoModel
 from Controller.ControlFunctions import ToDoListController
@@ -18,6 +18,7 @@ if __name__ == '__main__':
 
     parent_window = QMainWindow()
     widget = ToDoListController(parent_window, ToDoModel, Ui_ToDoLayout)
+    # attachQssEditor(parent_window)
     parent_window.show()
 
     sys.exit(app.exec_())
