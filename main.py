a4293d7acbfa33edd301a1e1bb590952d45d50a0
Matthew Mitchell <mazchoo@gmail.com>
2025-01-04 18:59:54 +0700

    Added ToDos

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ from UI.ToDoLayout import Ui_ToDoLayout
 from Models.ToDoModel import ToDoModel
 from Controller.ControlFunctions import ToDoListController
 
+# ToDo - Add the ability to save and load projects
+# ToDo - Allow editing of time and points on notes
+# ToDo - Add time recorder for notes
+# ToDo - Add linking of task to selected project
+# ToDo - Calculate totals for each project
+# ToDo - Add ability to delete projects
+# ToDo - Try using markdown for showing and allow editing using text
+# ToDo - Made it so unicode emojis can be saved and loaded in titles
 # ToDo - Allow editing of title to persist in saved data
 # ToDo - Disallow editing of an empty note
 
