5058306f85d0e2116050429cfe10acb7d0480bd5
Matthew Mitchell <mazchoo@gmail.com>
2025-01-04 14:23:59 +0700

    Updated UI for new features

--- main.py
+++ main.py
@@ -2,14 +2,13 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
-from Common.QtHelpers import loadQss  # , attachQssEditor
+from Common.QtHelpers import loadQss  #, attachQssEditor
 from UI.ToDoLayout import Ui_ToDoLayout
 from Models.ToDoModel import ToDoModel
 from Controller.ControlFunctions import ToDoListController
 
 # ToDo - Allow editing of title to persist in saved data
 # ToDo - Disallow editing of an empty note
-# ToDo - Allow the window to be resizeable
 
 
 if __name__ == '__main__':
