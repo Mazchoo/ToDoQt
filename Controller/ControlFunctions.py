46e67e19da8e4fdd59b581a7e0626f8376713329
Matthew Mitchell <mazchoo@gmail.com>
2025-01-25 16:29:14 +0700

    Removed new task text when click header

--- Controller/ControlFunctions.py
+++ Controller/ControlFunctions.py
@@ -231,6 +231,7 @@ def project_header_click(self, _clicked_index):
     self.model.project_list.set_selected_row(None)
     filter_available_tasks_for_selected_project(self.model, None)
 
+    self.layout.newTask_lineEdit.setText("")
     self.layout.newTask_lineEdit.setEnabled(False)
     self.layout.saveTaskChanges_pushButton.setEnabled(False)
     self.layout.deleteTask_pushButton.setEnabled(False)
