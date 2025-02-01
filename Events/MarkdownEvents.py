5ab77fa330b80253efbacf9a8da9696d9c5c8a64
Matthew Mitchell <mazchoo@gmail.com>
2025-02-01 11:10:16 +0700

    pylint refactor

--- Events/MarkdownEvents.py
+++ Events/MarkdownEvents.py
@@ -1,9 +1,11 @@
+''' Filtering events to turn markdown display on and off '''
 from PyQt5.QtCore import QObject, QEvent
 from PyQt5.QtWidgets import QTextEdit
 import markdown
 
 
 class MarkdownFocusHandler(QObject):
+    ''' When text edit selected, show raw markdown, when not selected show html of markdown '''
     def __init__(self, text_edit: QTextEdit):
         super().__init__()
         self.text_edit = text_edit
@@ -13,21 +15,26 @@ class MarkdownFocusHandler(QObject):
 
     @property
     def raw_markdown(self) -> str:
+        ''' Return current markdown '''
         return self._raw_markdown or ""
 
     @property
     def is_editing(self) -> bool:
+        ''' Return true if current markdown is being edited '''
         return self._is_being_edited
 
     def stop_editing(self):
+        ''' Turn off editing status '''
         self._is_being_edited = False
 
     def render_markdown(self):
+        ''' Convert raw edit text to display html of markdown '''
         self._raw_markdown = self.text_edit.toPlainText()
         rendered_html = markdown.markdown(self.raw_markdown)
         self.text_edit.setHtml(rendered_html)
 
     def eventFilter(self, obj: QObject, event: QEvent):
+        ''' Filter events so the selected focusing on text edit changes display '''
         if obj == self.text_edit and self.raw_markdown is not None:
             if event.type() == QEvent.FocusIn:
                 self.text_edit.setPlainText(self.raw_markdown)
--- Events/TimerEvents.py
+++ Events/TimerEvents.py
@@ -1,8 +1,10 @@
+''' Class links a timer that updates a time display every minute '''
 from PyQt5.QtWidgets import QTimeEdit
 from PyQt5.QtCore import QTimer
 
 
 class TimerEvents(QTimer):
+    ''' Timer connected to qt time edit '''
     def __init__(self, time_display: QTimeEdit):
         super().__init__()
         self.setInterval(60_000)  # Trigger once per minute
@@ -11,15 +13,18 @@ class TimerEvents(QTimer):
         self._recording = False
 
     def update_time(self):
+        ''' Increment time display '''
         current_time = self.time_display.time()
         new_time = current_time.addSecs(60)
         self.time_display.setTime(new_time)
 
     def stop_recording(self):
+        ''' Stop recording time '''
         self._recording = False
         self.stop()
 
     def toggle_recording(self) -> bool:
+        ''' Turn timing on and off '''
         self._recording = not self._recording
         if self._recording:
             self.start()
